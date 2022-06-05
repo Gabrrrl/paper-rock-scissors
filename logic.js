5bb7635985da4d6feb925c8fcaa92c48cc3224cf
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-04 22:31:49 -0700

    Added event listeners that fire when the form is submitted as well as preventing the form from refresing the page by using preventdefault. Took off autocomplete from the form and added logic to the playRound function that triggers if the playerchoice is anything other than paper, rock or scissors

--- logic.js
+++ logic.js
@@ -1,15 +1,22 @@
-// Computer Choice
+// Computer Random Choice function
 let computerPlay = () => {
     let choice = ['paper', 'rock', 'scissors']
     let random = () => {return Math.floor(Math.random() * choice.length)};
     return choice[random()]
 }
 
-// Global Variables for functions
-let playerSelection = "rock";
-// let computerSelection = computerPlay();
+// Targets the form with an id of submit and attaches an event listener that fires when enter is pressed in the submit the form, it then rund the playRound function
+document.getElementById('submit').addEventListener('submit', e => {
+    playRound(playerSelection, computerPlay());
+    e.preventDefault()
+})
 
-// event listener for button to run functions
+// targets the input element with an id of playerChoice and attaches an event listener that fires when text is written, the text then gets stored inside of the playerChoice var
+document.getElementById('playerChoice').addEventListener('change', e => {
+    playerSelection = document.getElementById('playerChoice').value.toLowerCase()
+})
+
+// event listener for button to call the playRound function which takes playerselection and computerPlay as the parameters to run the function
 document.getElementById('pick').addEventListener('click', e => {
     playRound(playerSelection, computerPlay());
 })
@@ -22,8 +29,11 @@ let playRound = (playerSelection, computerSelection) => {
         
     } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
         result = (`You Lose! ${computerSelection} beats ${playerSelection}`)
-    } else {
+    } else if (playerSelection == computerSelection) {
         result = (`Its a Tie!`)
+    } else {
+        alert('Please pick between Paper, Rock or Scissors!')
+        return
     }
     // creates p elements, adds text from the result of playRound function inside of them and appends them to the div by Id
     let pEl = document.createElement('p')
