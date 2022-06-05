fda7c6e9f141d1129a0f38322199002eb411bf4b
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-04 21:32:16 -0700

    Added player choice global variable, added a button event listener to run the play round function when clicked, the play round function runs the logic to determine the paper rock scissors winner then creates a p element and fills in the text content with the result of the playRound function and then appends the p element to the div element with the Id of choice

--- logic.js
+++ logic.js
@@ -1,12 +1,33 @@
-// console.log(input)
 // Computer Choice
 let computerPlay = () => {
-    let computerChoice = ['paper', 'rock', 'scissors']
-    let random = () => {return Math.floor(Math.random() * computerChoice.length)};
-    return computerChoice[random()]
+    let choice = ['paper', 'rock', 'scissors']
+    let random = () => {return Math.floor(Math.random() * choice.length)};
+    return choice[random()]
 }
 
-console.log(computerPlay())
+// Global Variables for functions
+let playerSelection = "rock";
+// let computerSelection = computerPlay();
 
+// event listener for button to run functions
+document.getElementById('pick').addEventListener('click', e => {
+    playRound(playerSelection, computerPlay());
+})
 
+// determines the winner between player choice and computer choice
+let playRound = (playerSelection, computerSelection) => {
+    let result = ``
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        result = (`You Win! ${playerSelection} beats ${computerSelection}`)
+        
+    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+        result = (`You Lose! ${computerSelection} beats ${playerSelection}`)
+    } else {
+        result = (`Its a Tie!`)
+    }
+    // creates p elements, adds text from the result of playRound function inside of them and appends them to the div by Id
+    let pEl = document.createElement('p')
+    pEl.textContent = result;
+    document.getElementById('choice').appendChild(pEl)
+}
 
