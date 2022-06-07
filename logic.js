be7e29335b9e0bfcaa7a07750dc9f41e1d1ae569
Gabriel Corral Cardenas <gabrielcorralcardenas@gmail.com>
2022-06-07 00:51:30 -0700

    Build UI with buttons instead of form

--- logic.js
+++ logic.js
@@ -9,22 +9,6 @@ let computerPlay = () => {
     return choice[random()]
 }
 
-// Targets the form with an id of submit and attaches an event listener that fires when enter is pressed in the submit the form, it then rund the playRound function
-document.getElementById('submit').addEventListener('submit', e => {
-    playRound(playerSelection, computerPlay());
-    e.preventDefault()
-})
-
-// targets the input element with an id of playerChoice and attaches an event listener that fires when text is written, the text then gets stored inside of the playerChoice var
-document.getElementById('playerChoice').addEventListener('change', e => {
-    playerSelection = document.getElementById('playerChoice').value.toLowerCase()
-})
-
-// event listener for button to call the playRound function which takes playerselection and computerPlay as the parameters to run the function
-document.getElementById('pick').addEventListener('click', e => {
-    playRound(playerSelection, computerPlay());
-})
-
 // determines the winner between player choice and computer choice, also adds 1 point to the playerpoints or computerpoints if there was a winner then calls the determineWinner function
 let playRound = (playerSelection, computerSelection) => {
     let result = ``
@@ -45,17 +29,30 @@ let playRound = (playerSelection, computerSelection) => {
     // creates p elements, adds text from the result of playRound function inside of them and appends them to the div by Id
     let pEl = document.createElement('p')
     pEl.textContent = result;
-    document.getElementById('choice').appendChild(pEl)
+    document.querySelector('#output').prepend(pEl)
 }
 
 // function that determines the winner after 5 wins and reloads the page after being acknowledgement
 let determineWinner = () => {
     if (playerScore == 5) {
         alert('You Won The Game!')
-        document.reload()
+        location.reload()
     } else if (computerScore == 5) {
         alert('You Lost The Game!')
-        document.reload()
+        location.reload()
     }
 }
 
+// attach event listeners for the buttons and get playerchoice from button value.
+document.querySelector('#rock').addEventListener('click', (e) => {
+    playerSelection = 'rock'
+    playRound(playerSelection, computerPlay());
+})
+document.querySelector('#paper').addEventListener('click', (e) => {
+    playerSelection = 'paper'
+    playRound(playerSelection, computerPlay());
+})
+document.querySelector('#scissors').addEventListener('click', (e) => {
+    playerSelection = 'scissors'
+    playRound(playerSelection, computerPlay());
+})
